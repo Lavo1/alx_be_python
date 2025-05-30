64238aa30998df551de8ffc119b578f6c7919d2d
Lavo1 <leratolerama3@gmail.com>
2025-05-30 15:52:39 +0200

    Create pattern_drawing.py

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,10 @@
+# pattern_drawing.py
+# Prompt User for Pattern Size
+size = int(input("Enter the size of the pattern: "))
+# Draw the Pattern
+row = 0
+while row < size:
+    for col in range(size):
+        print("*", end="")
+    print()  # Move to the next line after each row
+    row += 1
