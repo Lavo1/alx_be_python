7e3fb1d84b4d54462997ec9932070329442c65f1
Lavo1 <leratolerama3@gmail.com>
2025-06-15 20:31:05 +0200

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,6 +1,6 @@
 # bank_account.py
 class BankAccount:
-    def _init_ (self, initial_balance=0.0):
+    def __init__(self, initial_balance=0.0):
         self.__account_balance = initial_balance
 
     def deposit(self, amount):
